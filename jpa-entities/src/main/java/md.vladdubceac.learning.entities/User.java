8b7279e45537a0609e22d3547833a9e41edf3146
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-08-13 23:46:23 +0300

    Adding first module

---
+++ jpa-entities/src/main/java/md.vladdubceac.learning.entities/User.java
@@ -0,0 +1,25 @@
+package md.vladdubceac.learning.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
